e2aeef277a616c73fbebf5285132d46c29953bef
Francisco Cerqueira <up201905337@fe.up.pt>
2021-05-05 14:30:24 +0100

    Add Stock class

---
+++ src/market/Stock.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Francisco Cerqueira on 05/05/2021.
+//
+
+#include "Stock.h"
+
+int Stock::getQuantity(int productId) {
+    // TODO
+    return 0;
+}
+
+void Stock::setQuantity(int productId, int quantity) {
+    // TODO
+}
+
+void Stock::changeStock(int productId, int quantityVariation) {
+    // TODO
+}
---
+++ src/market/Stock.h
@@ -0,0 +1,18 @@
+#ifndef MARKET_ON_WHEELS_STOCK_H
+#define MARKET_ON_WHEELS_STOCK_H
+
+#include <map>
+
+class Stock {
+private:
+    std::map<int, int> inventory;  // (Product Id, Quantity)
+
+public:
+    Stock();
+
+    int getQuantity(int productId);
+    void setQuantity(int productId, int quantity);
+    void changeStock(int productId, int quantityVariation);
+};
+
+#endif //MARKET_ON_WHEELS_STOCK_H
