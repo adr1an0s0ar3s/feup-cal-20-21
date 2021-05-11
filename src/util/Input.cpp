4db2f6bcb3793239762c396f3bd6a9fcae663769
Vasco52 <64026324+Vasco52@users.noreply.github.com>
2021-05-11 23:12:09 +0100

    I've seen this somewhere

--- src/util/Input.cpp
+++ src/util/Input.cpp
@@ -1,17 +1,37 @@
 #include "Input.h"
 
 #include<iostream>
-#include<vector>
-
-bool allVehiclesFull(std::vector<Vehicle*>& vehicles) {
-    for (Vehicle *v : vehicles) {
-
-    }
-    return true;
-}
+#include <algorithm>
 
 void waitEnter() {
     std::cout << std::endl << "Press enter to continue..." << std::endl;
     std::string str;
     std::getline(std::cin, str);
+}
+
+std::string getRaw(){
+    std::string str;
+    std::cout << "> ";
+    std::getline(std::cin, str);
+    trimString(str);
+    std::cout << std::endl;
+    return str;
+}
+
+void trimString(std::string &str) {
+    // Remove whitespace from start and end of the string
+    auto front = std::find_if_not(str.begin(),str.end(),[](int c){
+        return std::isspace(c);
+    });
+    auto back = std::find_if_not(str.rbegin(),str.rend(),[](int c){
+        return std::isspace(c);
+    }).base();
+    str = back<=front ? std::string() : std::string(front, back);
+
+    // Convert multiple whitespaces into a single one
+    // For example "Avenida   dos      Aliados" would become "Avenida dos Aliados"
+    auto new_end = std::unique(str.begin(), str.end(), [](char a, char b){
+        return (a==b) && (a==' ');
+    });
+    str.erase(new_end, str.end());
 }
--- src/util/Input.h
+++ src/util/Input.h
@@ -1,12 +1,31 @@
 #ifndef MARKET_ON_WHEELS_INPUT_H
 #define MARKET_ON_WHEELS_INPUT_H
 
+#include <sstream>
 #include "../market/Vehicle.h"
 #include "../market/Order.h"
 
-#include <vector>
-
-bool allVehiclesFull(std::vector<Vehicle*>& vehicles);
 void waitEnter();
+std::string getRaw();
+void trimString(std::string &str);
+
+template <typename T>
+bool strToVar(const std::string &str, T &var){
+    std::istringstream ss{str};
+    ss >> var;
+    if(ss.fail() || !ss.eof()){
+        return false;
+    }
+    return true;
+}
+
+template <typename T>
+bool get(T &var){
+    std::string str = getRaw();
+    if(str.empty()){
+        return false;
+    }
+    return strToVar(str, var);
+}
 
 #endif //MARKET_ON_WHEELS_INPUT_H
